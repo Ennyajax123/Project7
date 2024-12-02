82f2e2b331645aab7e91a60ddcf8ca22ad048c60
Ennyajax123 <ajanaku.eniola123@gmail.com>
2024-12-02 15:44:46 +0000

    removed s3 bucket

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 #Create Backend
 
-terraform {
-  backend "s3" {
-    bucket  = "ennyajax1-bucket"
-    key     = "env/project7/terraform.tfstate"
-    region  = "eu-west-2"
-      }
-}
+#terraform {
+  #backend "s3" {
+   #bucket  = "ennyajax1-bucket"
+   # key     = "env/project7/terraform.tfstate"
+    #region  = "eu-west-2"
+    #  }
+#}
